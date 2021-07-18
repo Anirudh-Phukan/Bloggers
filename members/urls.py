bd83dd8ddc70d293d812377e9bd132774a409e86
Anirudh Phukan <anirudh.phukan@gmail.com>
2021-07-18 23:14:10 +0530

    final changes made

---
+++ members/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import UserResgisterView, UserEditView, PasswordsChangeView, ShowProfilePageView, EditProfilePageView, CreateProfilePageView
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('register/', UserResgisterView.as_view(),name="register"),
+    path('edit_profile/', UserEditView.as_view(),name="edit_profile"),
+    path('password/', PasswordsChangeView.as_view(template_name="registration/change-password.html"),name="change_password"),
+    path('password_success/', views.password_success,name="password_success"),
+    path('profile/<int:pk>', ShowProfilePageView.as_view(),name="show_profile_page"),
+    path('edit_profile_page/<int:pk>', EditProfilePageView.as_view(),name="edit_profile_page"),
+    path('create_profile_page/', CreateProfilePageView.as_view(),name="create_profile_page"),
+]
